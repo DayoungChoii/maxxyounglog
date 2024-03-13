a5be6920be66a3bba646e7063cb0e923effea90d
DayoungChoii <young12430@gmail.com>
2024-03-13 20:04:17 +0900

    Build: 사용하지 않는 어노테이션 삭제

--- order-app/api/src/main/kotlin/com/api/ApiApplication.kt
+++ order-app/api/src/main/kotlin/com/api/ApiApplication.kt
@@ -1,12 +1,10 @@
 package com.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 const val BASE_PACKAGE = "com"
 
-@ConfigurationPropertiesScan(basePackages = [BASE_PACKAGE])
 @SpringBootApplication(scanBasePackages = [BASE_PACKAGE])
 class ApiApplication
 
