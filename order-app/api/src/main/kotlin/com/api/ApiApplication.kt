c7fa75e6beda0973faf922231929fe28f8a5cf79
DayoungChoii <young12430@gmail.com>
2024-03-18 11:28:24 +0900

    Build: apllication.yml 민감정보 숨김 처리한다.

--- order-app/api/src/main/kotlin/com/api/ApiApplication.kt
+++ order-app/api/src/main/kotlin/com/api/ApiApplication.kt
@@ -2,10 +2,12 @@ package com.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 
 const val BASE_PACKAGE = "com"
 
 @SpringBootApplication(scanBasePackages = [BASE_PACKAGE])
+@PropertySource("classpath:/secure-aws.properties")
 class ApiApplication
 
 fun main(args: Array<String>) {
