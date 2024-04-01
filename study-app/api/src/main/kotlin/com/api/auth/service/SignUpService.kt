2ec8b3e1cb14d527d1434e5b968be6b9614b24c6
DayoungChoii <young12430@gmail.com>
2024-04-02 02:55:40 +0900

    Feat: [#1] 회원가입 시 유저 포인트 1000 포인트를 지급힌다.

--- study-app/api/src/main/kotlin/com/api/auth/service/SignUpService.kt
+++ study-app/api/src/main/kotlin/com/api/auth/service/SignUpService.kt
@@ -1,9 +1,11 @@
 package com.api.auth.service
 
+import com.api.auth.constant.SignUpStatus
 import com.api.auth.dto.SignUpRequest
 import com.api.auth.exception.AuthException
 import com.api.auth.exception.AuthExceptionType
-import com.api.auth.constant.SignUpStatus
+import com.api.common.const.SIGN_UP_POINT
+import com.async.user.UserPointService
 import com.rds.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,7 +13,8 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class SignUpService (
     private val userRepository: UserRepository,
-    private val encryptor: Encryptor
+    private val encryptor: Encryptor,
+    private val userPointService: UserPointService
 ){
 
     @Transactional
@@ -22,11 +25,11 @@ class SignUpService (
         try {
             val encryptedPassword = encryptor.encrypt(signUpRequest.password)
             signUpRequest.encryptPassword(encryptedPassword)
-            userRepository.save(signUpRequest.toUser());
+            val user = userRepository.save(signUpRequest.toUser());
+            userPointService.createPoint(user.id, SIGN_UP_POINT)
         } catch (e: RuntimeException) {
             throw AuthException(AuthExceptionType.SIGN_UP_FAIL, e)
         }
-
         return SignUpStatus.SUCCESS;
     }
 
---
+++ study-app/api/src/main/kotlin/com/api/common/const/PointConst.kt
@@ -0,0 +1,3 @@
+package com.api.common.const
+
+const val SIGN_UP_POINT: Int = 1000
