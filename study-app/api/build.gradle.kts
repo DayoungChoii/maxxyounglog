2d25fa887cd7c4f319a83ed7ba06c080cd68d023
DayoungChoii <young12430@gmail.com>
2024-04-07 21:10:22 +0900

    Build: querydsl 설정을 추가한다.

--- study-app/api/build.gradle.kts
+++ study-app/api/build.gradle.kts
@@ -1,20 +1,11 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
 plugins {
 	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.21"
 	kotlin("plugin.spring") version "1.9.21"
 	kotlin("plugin.jpa") version "1.9.21"
-}
+	kotlin("kapt")
 
-tasks.getByName<BootJar>("bootJar") {
-	enabled = false
-}
-
-tasks.getByName<Jar>("jar") {
-	enabled = true
-	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 object Versions {
@@ -28,7 +19,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
 	implementation("org.springframework.boot:spring-boot-starter-security")
@@ -39,6 +29,9 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
+	//querydsl
+	implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	kapt ("com.querydsl:querydsl-apt:5.0.0:jakarta")
 
 	runtimeOnly("com.h2database:h2")
 
