ec74bfa8fd75790ab6cf14b9a570251e83197e50
DayoungChoii <young12430@gmail.com>
2024-04-09 02:32:01 +0900

    Fix: 회원 가입 시 테스트 코드의 이벤트 의존성을 추가한다.

--- study-app/api/src/test/kotlin/com/api/auth/service/SignUpServiceTest.kt
+++ study-app/api/src/test/kotlin/com/api/auth/service/SignUpServiceTest.kt
@@ -1,20 +1,23 @@
 package com.api.auth.service
 
-import com.api.auth.dto.SignUpRequest
 import com.api.auth.constant.SignUpStatus
+import com.api.auth.dto.SignUpRequest
 import com.appmattus.kotlinfixture.kotlinFixture
 import com.rds.user.domain.User
+import com.rds.user.event.UserSignedUpEvent
 import com.rds.user.repository.UserRepository
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import org.springframework.context.ApplicationEventPublisher
 
 class SignUpServiceTest: BehaviorSpec({
 
     val userRepository: UserRepository by lazy { mockk<UserRepository>() }
     val encryptor: Encryptor by lazy { mockk<Encryptor>()}
-    val signUpService: SignUpService by lazy { SignUpService(userRepository, encryptor)}
+    val eventPublisher: ApplicationEventPublisher by lazy { mockk<ApplicationEventPublisher>() }
+    val signUpService: SignUpService by lazy { SignUpService(userRepository, encryptor, eventPublisher)}
     val fixture by lazy { kotlinFixture() }
 
     `given`("회원 가입할 때") {
@@ -30,6 +33,7 @@ class SignUpServiceTest: BehaviorSpec({
             every { userRepository.findByEmail(request.email) } returns null
             every { encryptor.encrypt(request.password) } returns "encryptedPassword"
             every { userRepository.save(any(User::class)) } returns fixture<User>()
+            every { eventPublisher.publishEvent(any(UserSignedUpEvent::class)) } returns Unit
             `then`("완료 상태를 반환한다.") {
                 signUpService.signUp(request) shouldBe SignUpStatus.SUCCESS
             }
