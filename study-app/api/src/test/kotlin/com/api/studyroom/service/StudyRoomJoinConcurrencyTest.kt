6c730015685c7431da2e6f020705157f2deeb50a
DayoungChoii <young12430@gmail.com>
2024-04-12 00:49:01 +0900

    Feat: [#2] 스터디방 가입 시 동시성 이슈 테스트 코드를 작성한다.

---
+++ study-app/api/src/test/kotlin/com/api/studyroom/service/StudyRoomJoinConcurrencyTest.kt
@@ -0,0 +1,62 @@
+package com.api.studyroom.service
+
+import com.api.common.IntegrationTest
+import com.appmattus.kotlinfixture.kotlinFixture
+import com.rds.category.domain.Category
+import com.rds.category.repository.CategoryRepository
+import com.rds.studyroom.domain.StudyRoom
+import com.rds.studyroom.domain.StudyRoomState
+import com.rds.studyroom.repository.StudyRoomRepository
+import com.rds.studyroom.repository.UserStudyRoomRepository
+import com.rds.user.domain.User
+import com.rds.user.domain.UserState
+import com.rds.user.repository.UserRepository
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+@IntegrationTest
+class StudyRoomJoinConcurrencyTest @Autowired constructor(
+    val studyRoomService: StudyRoomService,
+    val studyRoomRepository: StudyRoomRepository,
+    val userStudyRoomRepository: UserStudyRoomRepository,
+    val categoryRepository: CategoryRepository,
+    val userRepository: UserRepository
+) {
+
+    val fixture = kotlinFixture()
+
+    @Test
+    fun studyRoomJoinConcurrencyTest() {
+        //given
+        val threadCount = 50
+
+        val category = categoryRepository.save(fixture<Category>())
+        val studyRoom = studyRoomRepository.save(fixture<StudyRoom>() {
+        property(StudyRoom::category) { category }
+        property(StudyRoom::state) { StudyRoomState.ACTIVATED }
+        })
+        val studyRoomId = studyRoom.id
+        val users = (1..threadCount).map { userRepository.save(fixture<User> { property(User::state) { UserState.ACTIVATED } }) }
+
+        val executorService: ExecutorService = Executors.newFixedThreadPool(32)
+        val latch = CountDownLatch(threadCount)
+
+        //when
+        for (user in users) {
+            executorService.execute {
+                val response = studyRoomService.joinStudyRoom(studyRoomId, user.id)
+                println(">>> response: $response")
+                latch.countDown()
+            }
+        }
+        latch.await()
+
+        //then
+        val userCount = userStudyRoomRepository.countByStudyRoom(studyRoomId)
+        Assertions.assertThat(userCount).isEqualTo(10)
+    }
+}
