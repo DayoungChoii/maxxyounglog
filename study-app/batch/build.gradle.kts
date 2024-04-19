4b8a9858157b6f2e09ba7c83e7c40319dc709c37
DayoungChoii <young12430@gmail.com>
2024-04-19 11:56:05 +0900

    Build: 배치 모듈에 querydsl 의존성을 추가한다.

--- study-app/batch/build.gradle.kts
+++ study-app/batch/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.9.21"
 	kotlin("plugin.spring") version "1.9.21"
 	kotlin("plugin.jpa") version "1.9.21"
+	kotlin("kapt")
 }
 
 tasks.getByName<BootJar>("bootJar") {
@@ -25,6 +26,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-batch")
+	//querydsl
+	implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	kapt ("com.querydsl:querydsl-apt:5.0.0:jakarta")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
