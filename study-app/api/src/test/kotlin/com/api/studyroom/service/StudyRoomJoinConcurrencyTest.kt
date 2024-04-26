25f9bde4f5e692325841497d8d8cc5dc2e2ef422
DayoungChoii <young12430@gmail.com>
2024-04-26 15:30:47 +0900

    Fix: 스터디방 가입 동시성 테스트에서 스프링 이벤트(포인트 관련)를 모킹한다.

--- study-app/api/src/test/kotlin/com/api/studyroom/service/StudyRoomJoinConcurrencyTest.kt
+++ study-app/api/src/test/kotlin/com/api/studyroom/service/StudyRoomJoinConcurrencyTest.kt
@@ -1,33 +1,42 @@
 package com.api.studyroom.service
 
 import com.api.common.IntegrationTest
+import com.api.common.RedisLockExecutor
+import com.api.studyroom.repository.StudyRoomQueryRepository
 import com.appmattus.kotlinfixture.kotlinFixture
 import com.rds.category.domain.Category
 import com.rds.category.repository.CategoryRepository
 import com.rds.studyroom.domain.StudyRoom
 import com.rds.studyroom.domain.StudyRoomState
+import com.rds.studyroom.event.StudyRoomJoinedEvent
 import com.rds.studyroom.repository.StudyRoomRepository
 import com.rds.studyroom.repository.UserStudyRoomRepository
 import com.rds.user.domain.User
 import com.rds.user.domain.UserState
 import com.rds.user.repository.UserRepository
+import io.mockk.every
+import io.mockk.mockk
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationEventPublisher
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 @IntegrationTest
 class StudyRoomJoinConcurrencyTest @Autowired constructor(
-    val studyRoomService: StudyRoomService,
     val studyRoomRepository: StudyRoomRepository,
     val userStudyRoomRepository: UserStudyRoomRepository,
     val categoryRepository: CategoryRepository,
     val userRepository: UserRepository,
-    val studyRoomServiceLockFacade: StudyRoomServiceLockFacade
+    val studyRoomJoinValidator: StudyRoomJoinValidator,
+    val redisLockExecutor: RedisLockExecutor
+
 ) {
 
+    val eventPublisher: ApplicationEventPublisher = mockk<ApplicationEventPublisher>()
+    val studyRoomQueryRepository = mockk<StudyRoomQueryRepository>()
     val fixture = kotlinFixture()
     val threadCount = 50
 
@@ -35,6 +44,9 @@ class StudyRoomJoinConcurrencyTest @Autowired constructor(
     @Test
     fun studyRoomJoinConcurrencyTest() {
         //given
+        val studyRoomService = createStudyRoomService()
+        every { eventPublisher.publishEvent(any(StudyRoomJoinedEvent::class)) } returns Unit
+
         val (studyRoomId, users) = saveEntitiesForStudyRoom()
         val executorService: ExecutorService = Executors.newFixedThreadPool(32)
         val latch = CountDownLatch(threadCount)
@@ -54,9 +66,25 @@ class StudyRoomJoinConcurrencyTest @Autowired constructor(
         Assertions.assertThat(userCount).isGreaterThanOrEqualTo(10)
     }
 
+    private fun createStudyRoomService() = StudyRoomService(
+        studyRoomRepository = studyRoomRepository,
+        categoryRepository = categoryRepository,
+        studyRoomQueryRepository = studyRoomQueryRepository,
+        studyRoomJoinValidator = studyRoomJoinValidator,
+        userStudyRoomRepository = userStudyRoomRepository,
+        userRepository = userRepository,
+        eventPublisher = eventPublisher
+    )
+
     @Test
     fun studyRoomJoinConcurrencyRedissonTest() {
         //given
+        val studyRoomServiceLockFacade = StudyRoomServiceLockFacade(
+            studyRoomService = createStudyRoomService(),
+            redisLockExecutor = redisLockExecutor
+        )
+        every { eventPublisher.publishEvent(any(StudyRoomJoinedEvent::class)) } returns Unit
+
         val (studyRoomId, users) = saveEntitiesForStudyRoom()
         val executorService: ExecutorService = Executors.newFixedThreadPool(32)
         val latch = CountDownLatch(threadCount)
